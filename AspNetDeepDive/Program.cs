2cc078ac4a6792495b1d7dfde415361010742438
alexesp <aleizaes@hotmail.com>
2025-03-22 11:58:11 +0100

    Crear middleware

--- AspNetDeepDive/Program.cs
+++ AspNetDeepDive/Program.cs
@@ -4,6 +4,33 @@ using Microsoft.AspNetCore.DataProtection.KeyManagement;
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
+//Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+   await context.Response.WriteAsync("Middleware #1: Before calling next\r\n");
+
+   await next(context);
+
+    await context.Response.WriteAsync("Middleware #1: After calling next\r\n");
+});
+//MIddleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
+
+    await next(context);
+
+    await context.Response.WriteAsync("Middleware #2: After calling next\r\n");
+});
+//Middleware 3
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #3: Before calling next\r\n");
+
+    await next(context);
+
+    await context.Response.WriteAsync("Middleware #3: After calling next\r\n");
+});
 //app.MapGet("/", () => "Hello World!");
 
 app.Run(async (HttpContext context) =>
