c2a39f632c30652727cce061bfac7f041c262804
alexesp <aleizaes@hotmail.com>
2025-03-27 15:56:07 +0100

    Implimentar crud

--- Routing/Program.cs
+++ Routing/Program.cs
@@ -7,20 +7,24 @@ var app = builder.Build();
 //    await context.Response.WriteAsync("Get employees");
 //});
 
-app.Use(async (context, next) =>
-{
-    await next(context);
-});
+//app.Use(async (context, next) =>
+//{
+//    await next(context);
+//});
 
 app.UseRouting();
 
-app.Use(async (context, next) =>
-{
-    await next(context);
-});
+//app.Use(async (context, next) =>
+//{
+//    await next(context);
+//});
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapGet("/", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("Welcome to the home page.");
+    });
     endpoints.MapGet("/employees", async (HttpContext context) =>
     {
         await context.Response.WriteAsync("Get employees");
@@ -33,14 +37,19 @@ app.UseEndpoints(endpoints =>
     {
         await context.Response.WriteAsync("Update an employees");
     });
-    endpoints.MapDelete("/employees/{id}", async (HttpContext context) =>
+    endpoints.MapDelete("/employees/{id:int}", async (HttpContext context) =>
     {
         await context.Response.WriteAsync($"Delete the employees: {context.Request.RouteValues["id"]}");
     });
-    endpoints.MapGet("/{category=shirts}/{size=medium}/{id=0}", async (HttpContext context) =>
-    {
-        await context.Response.WriteAsync($"Get categories: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]}");
-    });
+    //endpoints.MapGet("/{category=shirts}/{size=medium}/{id=0}", async (HttpContext context) =>
+    //{
+    //    await context.Response.WriteAsync($"Get categories: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]}");
+    //});
+    //endpoints.MapGet("/{category=shirts}/{size=medium}/{id?}", async (HttpContext context) =>
+    //{
+    //    await context.Response.WriteAsync($"Get categories: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]}");
+    //});
 });
+
 
 app.Run();
